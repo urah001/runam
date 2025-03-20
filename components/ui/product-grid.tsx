41deda6b69efb5f6a8172e522b5a2f9b65c44c7f
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2025-03-20 12:09:13 +0100

    changed the money sign

--- components/ui/product-grid.tsx
+++ components/ui/product-grid.tsx
@@ -151,7 +151,7 @@ function ProductCard({ product }: {product : Product}) {
         <Link href={`/products/${product.slug}`} className="hover:underline">
           <h3 className="font-medium text-lg mb-2">{product.name}</h3>
         </Link>
-        <p className="font-bold">${product.price.toFixed(2)}</p>
+        <p className="font-bold">₦{product.price.toFixed(2)}</p>
       </CardContent>
       <CardFooter className="p-4 pt-0">
         <Button className="w-full" onClick={() => addItem({ ...product, quantity: 1 })}>
